601ad2cfe2ed215f5f37a673911137546185f399
ifpilucas <ifpilucas@gmail.com>
2019-02-01 15:24:10 -0300

    Recriando Projeto do Zero

---
+++ lrenu/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LrenuConfig(AppConfig):
+    name = 'lrenu'
---
+++ lrenu/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class user(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    passwd = models.CharField(max_length=200)
