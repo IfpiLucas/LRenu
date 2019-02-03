efea25c100050c36cc4f70ff3f02439a9a213c0b
IfpiLucas <ifpilucas@gmail.com>
2019-02-03 11:30:00 -0300

    Add templates and statics files

--- settings/urls.py
+++ settings/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from lrenu.views import index
+from lrenu.views import index, register, login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('registra', register),
+    path('login', login),
 
 ]
